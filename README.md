edea862bf1b989a911e89163531bbce43687cf84
Sam Maxwell <maxwell.g.sam@gmail.com>
2024-10-12 23:59:24 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# weekly-activity-tracker
-A tool for tracking weekly activities and learning to form part of a CPD record for professional development. Allows you to set and check off weekly goals and targets, as well as record achievements and activities completed throughout the week.
+# Weekly Activity Tracker
+
+A tool for tracking weekly activities and learning to form part of a CPD record for professional development.
+Allows you to set and check off weekly goals and targets, as well as record achievements and activities completed throughout the week.
+
+
---
+++ WeeklyActivityTracker.Models/WeeklyTarget/WeeklyTarget.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * AUTHOR: Sam Maxwell
+ * DATE CREATED: 12/10/2024
+ * 
+ * DESCRIPTION: A model that describes Weekly Target information.
+ */
+
+using System.ComponentModel.DataAnnotations;
+
+namespace WeeklyActivityTracker.Models.WeeklyTarget
+{
+    public class WeeklyTarget
+    {
+        // PROPERTIES
+        /// <summary>
+        /// The unique identifier of the Weekly Target entry.
+        /// </summary>
+        [Key]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The name of the Weekly Target.
+        /// </summary>
+        public string TargetName { get; set; }
+
+        /// <summary>
+        /// A description of the Weekly Target.
+        /// </summary>
+        public string TargetDecription { get; set; }
+
+        /// <summary>
+        /// The DateTime that the Target was created at.
+        /// </summary>
+        public DateTime TargetCreatedTimestamp { get; set; }
+
+        /// <summary>
+        /// The DateTime that the Target must be completed at.
+        /// </summary>
+        public DateTime TargetCompleteByTimestamp { get; set; }
+
+        /// <summary>
+        /// The DateTime that the Targt was completed at.
+        /// </summary>
+        public DateTime TargetCompletedTimestamp { get; set; }
+    }
+}
