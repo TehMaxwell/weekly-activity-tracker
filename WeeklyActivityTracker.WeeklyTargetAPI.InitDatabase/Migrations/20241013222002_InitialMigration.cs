1623cf0ea6927b753b67bffaa9108384feb964e8
Sam Maxwell <maxwell.g.sam@gmail.com>
2024-10-13 23:22:43 +0100

    Feature/add GitHub action container build weekly target init database (#8)
    * Add docker build pipeline for WeeklyTargetAPI.InitDatabase
    
    * Changed image tag names
    
    * Moved new action to correct folder
    
    * Added migrations for initdatabase

---
+++ WeeklyActivityTracker.WeeklyTargetAPI.InitDatabase/Migrations/20241013222002_InitialMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeeklyActivityTracker.WeeklyTargetAPI.InitDatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeeklyTargets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TargetName = table.Column<string>(type: "text", nullable: false),
+                    TargetDecription = table.Column<string>(type: "text", nullable: false),
+                    TargetCreatedTimestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TargetCompleteByTimestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TargetCompletedTimestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeeklyTargets", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeeklyTargets");
+        }
+    }
+}
