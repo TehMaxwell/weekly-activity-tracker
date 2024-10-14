c5365c354e5d8abbc9d3110f9dd781dccb168ef0
Sam Maxwell <maxwell.g.sam@gmail.com>
2024-10-15 00:37:31 +0100

    Added sample next.js frontend

---
+++ weeklyactivitytracker.frontend/src/app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <header>
+        <h1>I'm starting my own thing now.</h1>
+      </header>
+
+      <div>
+        <p>
+          
+        </p>
+      </div>
+
+      <footer>
+        <h2>This is the footer.</h2>
+      </footer>
+    </>
+  );
+}
