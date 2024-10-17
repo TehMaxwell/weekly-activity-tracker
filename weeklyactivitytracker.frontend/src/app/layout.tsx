d74603943c58a342b754a97d22c65c81872cbb27
Sam Maxwell <maxwell.g.sam@gmail.com>
2024-10-17 01:14:26 +0100

    Added sidebar

--- weeklyactivitytracker.frontend/src/app/layout.tsx
+++ weeklyactivitytracker.frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { notoSans } from "./ui/fonts";
 import "./globals.css";
+import SideNav from "./ui/sidenav";
 
 export const metadata: Metadata = {
   title: "Weekly Activity Tracker",
@@ -16,6 +17,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${notoSans.className} antialiased`}>
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-64">
+            <SideNav />
+          </div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </div>
         {children}
       </body>
     </html>
--- weeklyactivitytracker.frontend/src/app/page.tsx
+++ weeklyactivitytracker.frontend/src/app/page.tsx
@@ -1,20 +1,12 @@
 import Image from "next/image";
 import { notoSansMono } from './ui/fonts';
+import SideNav from './ui/sidenav';
 
 export default function Home() {
   return (
-    <>
-      <header>
-        <Image 
-          src='/yorkshireyoda.jpg'
-          width={200}
-          height={152}
-          className="hidden md:block"
-          alt="Be reyt it will"
-        />
-        <h1>I'm starting my own thing now.</h1>
+    <div className="flex h-full flex-col py-4 md:px-5">
+      <header className="mb-2 flex h-20 items-end justify-start rounded-md bg-orange-600 p-4 md:h-40">
       </header>
-
       <main className={`${notoSansMono.className} text-gray-300 flex min-h-screen flex-col p-5`}>
         Ding dong, bing bong.
       </main>
@@ -22,6 +14,6 @@ export default function Home() {
       <footer>
         <h2>This is the footer.</h2>
       </footer>
-    </>
+    </div>
   );
 }
